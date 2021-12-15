2ccfffdbddf853d680953685c8ebe94622a8961d
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-12-15 15:47:03 +0200

    add basic project layout and data fetching

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+import {useQuery} from "react-query";
+
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge'
+
+import {Wrapper} from './App.styles';
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> => (
+  await (await fetch('https://fakestoreapi.com/products')).json()
+);
+
+const App = () => {
+  const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts);
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = () => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress/>
+  return (
+    <div className='App'>
+      Hello
+    </div>
+  );
+};
+
+export default App;
