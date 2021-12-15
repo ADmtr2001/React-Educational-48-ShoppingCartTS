7f55a41ca9e6ab034716dbff902c836e1d2923f2
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-12-15 15:59:56 +0200

    add products list rendering and Item component with related styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
+
 import {useQuery} from "react-query";
 
+import Item from './Item/Item';
 import Drawer from "@material-ui/core/Drawer";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Grid from '@material-ui/core/Grid';
@@ -28,15 +30,21 @@ const App = () => {
 
   const getTotalItems = () => null;
 
-  const handleAddToCart = () => null;
+  const handleAddToCart = (clickedItem: CartItemType) => null;
 
   const handleRemoveFromCart = () => null;
 
   if (isLoading) return <LinearProgress/>
   return (
-    <div className='App'>
-      Hello
-    </div>
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart}/>
+          </Grid>
+        )))}
+      </Grid>
+    </Wrapper>
   );
 };
 
